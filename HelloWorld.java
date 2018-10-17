aa096341b955129c1c20e1e194e08308e35f3151
QueenMeow27 <44196044+QueenMeow27@users.noreply.github.com>
2018-10-17 21:49:45 +1100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       for(int i=0; i<10;i++)
-      System.out.println("Hello, World");
+      System.out.println("Snow White");
    }
 }
